0b0494736de1755d3c4adcb06e29bd9831543126
0413960 <0413960@cstjean.qc.ca>
2020-09-03 18:43:24 -0400

    ajout boucle de jeu avec window.requestAnimationFrame()

--- index.js
+++ index.js
@@ -1,57 +1,99 @@
+/*
+    Auteur: Yanik Sweeney
+    script permettant de gérer une boucle de jeu pour faire des animations et des déplacements
+*/
+const keyQueue = [];
+const updateQueue = [];
+
 let positionX = 0;
 let positionY = 0;
 let moveSpeed = 20;
 let controller;
 let turnOff = false;
-let deltaTime = 0;
-const keyQueue = [];
-const updateQueue = [];
+let secondsPassed;
+let oldTimeStamp;
+let fps;
 
-/*
-    auteur: Yanik Sweeney
-    Permet à la page de recevoir les évènements des key presses
-*/
 document.onkeypress = function (evt) {
     evt = evt || window.event;
     let charCode = evt.keyCode || evt.which;
     let charStr = String.fromCharCode(charCode);
     //moveController(charStr);
+    keyQueue.push(charStr);
 };
 
 function onStart() {
     controller = document.getElementById('control');
     controller.style.bottom = "0px";
     controller.style.left = "0px";
+
+    window.requestAnimationFrame(gameLoop);
+
+}
+
+function updateMovement() {
+    moveController(keyQueue.pop());
+    // To flush queue
+    while (keyQueue.length > 0) {
+        keyQueue.pop();
+    }
+    positionY -= 1;
+    if (positionY < 0) {
+        positionY = 0;
+    }
+    if (positionX < 0) {
+        positionX = 0;
+    }
+}
+
+
+function draw() {
+    controller.style.bottom = positionY + "px";
+    controller.style.left = positionX + "px";
 }
 
 function moveController(keyPress) {
     switch (keyPress) {
         case "w":
             positionY += moveSpeed;
-            controller.style.bottom = positionY + "px";
             break;
         case "a":
             positionX -= moveSpeed;
-            controller.style.left = positionX + "px";
             break;
         case "s":
             positionY -= moveSpeed;
-            controller.style.bottom = positionY + "px";
             break;
         case "d":
             positionX += moveSpeed;
-            controller.style.left = positionX + "px";
             break;
         default:
             break;
     }
 }
 
-function loopProcessor() {
-    let time = Date.now();
-    while (!turnOff) {
-        
-    }
+function updateFpsMeter() {
+    let meter = document.getElementById("fpsMeter");
+    meter.innerText = fps + " FPS";
+}
+
+
+function gameLoop(timeStamp) {
+
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed);
+
+    updateFpsMeter();
+    // Perform the drawing operation
+    updateMovement();
+
+    draw();
+
+    // The loop function has reached it's end. Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
 }
 
 
